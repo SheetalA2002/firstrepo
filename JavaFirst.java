7c8d626b7132b4fd8ec39508e9b575732ea25386
yogitauttur <47716591+yogitauttur@users.noreply.github.com>
2023-01-16 12:34:11 +0530

    Update JavaFirst.java

--- JavaFirst.java
+++ JavaFirst.java
@@ -2,6 +2,6 @@ import java.util.*;
 class JavaFirst{
 	void first(){
 		System.out.println("Hello World");
-		System.out.println("Uttur Yogita");
+                System.out.println(" Yogita ");
 	}
 }
