c4f78c7e6f1f193af6ea7055cabb0845c53e7a3c
Balmani-20 <122628801+Balmani-20@users.noreply.github.com>
2023-01-16 12:07:19 +0530

    Update JavaFirst.java

--- JavaFirst.java
+++ JavaFirst.java
@@ -6,7 +6,7 @@ class JavaFirst{
 		System.out.println("Aishwarya Annam");
 		System.out.println("Anjali Guda");
 		System.out.println("Shrutika Kadgi");
-		
+		System.out.println("Balmani Battin");
 	}
 
 }
